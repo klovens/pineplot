27f91fd3668cdc96689de8d2e06706fd50048d5f
Daniel Hogan <daniel.hogan@usask.ca>
2019-07-10 23:50:52 -0600

    add tests for symmetric_matrix_generator

---
+++ tests/testthat/test-symmetric_matrix_generator.R
@@ -0,0 +1,21 @@
+context("Testing symmetric matrices")
+
+test_that("symmetric_matrix_generator is creating symmetric matrices", {
+	m <- matrix(runif(10*3), 3, 10)
+	r <- symmetric_matrix_generator(m, 1)
+	expect_equal(r, t(r))
+	m <- matrix(runif(100), 1, 100)
+	r <- symmetric_matrix_generator(m, 1)
+	expect_equal(r, t(r))
+	m <- matrix(runif(100), 100, 1)
+	r <- symmetric_matrix_generator(m, 1)
+	expect_equal(r, t(r))
+})
+
+test_that("Check if symmetric matrices converted to triangular matrices", {
+
+})
+
+test_that("Non symmetric matrix causes message", {
+
+})
