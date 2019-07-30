bf00e02f0e1c4f4c1acce3f7e778a370d1ff3c85
Daniel Hogan <daniel.hogan@usask.ca>
2019-07-29 19:37:55 -0600

    update tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,6 @@
 library(testthat)
 library(pineplot)
+library(gridExtra)
+library(gtable)
 
 test_check("pineplot")
--- tests/testthat/test-generate_pineplot.R
+++ tests/testthat/test-generate_pineplot.R
@@ -25,3 +25,19 @@ test_that("pineplot accomodates labels", {
   ms <- lapply(ms, `colnames<-`, rep('a very long label', 7))
   ggsave('../figs/generate_pineplot-4.pdf', generate_pineplot(ms), height=12)
 })
+
+test_that("pineplot is generated without legend", {
+  # check tests/figs directory to validate output
+  ms <- replicate(4, outer(-3:3, -3:3, "+"), simplify = FALSE)
+  ggsave('../figs/generate_pineplot-5.pdf', generate_pineplot(ms, legend=FALSE), height=12)
+})
+
+test_that("pineplot is generated and legend is returned with the pineplot object", {
+  # check tests/figs directory to validate output
+  ms <- replicate(4, outer(-3:3, -3:3, "+"), simplify = FALSE)
+  pp1 <- generate_pineplot(ms, legend=F, legend_scale=0.) # TODO legend_scale
+  pp2 <- generate_pineplot(ms, legend=F, legend_scale=0.) # TODO legend_scale
+  pdf('../figs/generate_pineplot-6.pdf')
+  grid.arrange(pp1, pp2, pp1$legend_grob, layout_matrix=rbind(c(1, 2), c(3, 3)), heights=c(5, 1))
+  dev.off()
+})
